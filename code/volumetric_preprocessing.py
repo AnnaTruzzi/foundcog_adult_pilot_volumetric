a255304bfd821b53a2a488bee9f2bac2896d6d9a
AnnaTruzzi <anna.truzzi91@gmail.com>
2021-08-13 15:27:01 +0000

    try to get the roi extraction to work

--- code/volumetric_preprocessing.py
+++ code/volumetric_preprocessing.py
@@ -7,18 +7,88 @@ from nipype.interfaces import fsl
 from nipype.interfaces.ants import Registration
 from nipype.utils.filemanip import Path
 from nipype.interfaces.ants import ApplyTransforms
+from nipype.interfaces import afni as afni
 
 s3=boto3.client('s3')
 bucket='foundcog-adult-pilot'
-sub_list = ['sub-02']
+sub_list = ['sub-03']
 
-#tasks_list = ['resting_run-001', 'video_run-001', 'video_run-002']
-tasks_list = ['resting_run-001']
+#tasks_list = ['resting_run-1', 'video_run-1', 'video_run-2']
+tasks_list = ['resting_run-1']
+count = 1
 
 for sub in sub_list:
     for task in tasks_list :
         temp_pth = '/home/annatruzzi/foundcog_adult_pilot_volumetric/temp/'
-        s3.download_file(bucket, f'foundcog-adult-pilot-2/bids/{sub}/ses-001/func/{sub}_ses-001_task-{task}_bold.nii.gz', f'{sub}_ses-001_task-{task}_bold.nii.gz')
+        s3.download_file(bucket, f'foundcog-adult-pilot-2/deriv-2_topup/fmriprep/{sub}/ses-001/func/{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz', f'{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz')
+        s3.download_file(bucket, f'foundcog-adult-pilot-2/deriv-2_topup/fmriprep/{sub}/ses-001/func/{sub}_ses-001_task-{task}_from-T1w_to-scanner_mode-image_xfm.txt', f'{sub}_ses-001_task-{task}_from-T1w_to-scanner_mode-image_xfm.txt')
+        os.system(f'mv {sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz ../temp/')
+        os.system(f'mv {sub}_ses-001_task-{task}_from-T1w_to-scanner_mode-image_xfm.txt ../temp/')
+
+        if count == 0:
+            ## Register atlas to pilot space
+            print('Registering FSL MNI to pilot space')
+            regflt = fsl.FLIRT()
+            regflt.inputs.in_file = Path('/dhcp/rhodri_registration/atlases/schaefer/Parcellations/MNI/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm.nii.gz')
+            regflt.inputs.reference = os.path.join(temp_pth, f'{sub}_ses-001_run-1_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz')
+            regflt.inputs.out_file = os.path.join(temp_pth, 'FSLMNI_to_MNI152NLin2009cAsym.nii.gz')
+            regflt.inputs.out_matrix_file = os.path.join(temp_pth, f'{sub}_ses-001_template_to_native.mat')
+            res = regflt.run()
+
+            print('Registering FSL MNI to pilot space')
+            regflt = fsl.FLIRT()
+            regflt.inputs.in_file = Path('/dhcp/rhodri_registration/atlases/schaefer/Parcellations/MNI/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm.nii.gz')
+            regflt.inputs.reference = os.path.join(temp_pth, f'{sub}_ses-001_run-1_space-_desc-brain_mask.nii.gz')
+            regflt.inputs.out_file = os.path.join(temp_pth, 'FSLMNI_to_MNI152NLin2009cAsym.nii.gz')
+            regflt.inputs.out_matrix_file = os.path.join(temp_pth, f'{sub}_ses-001_template_to_native.mat')
+            res = regflt.run()
+
+        #os.system('invwarp -r ../temp/{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz -w  -o')
+        #print(f'{sub} {task} - Working on applywarp...')
+        #os.system(f'fslascii2img ../temp/{sub}_ses-001_task-{task}_from-T1w_to-scanner_mode-image_xfm.txt')
+        #os.system(f'applywarp -i /dhcp/rhodri_registration/atlases/schaefer/Parcellations/MNI/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm.nii.gz -o ../temp/{sub}_{task}_Schaefer_resliced.nii.gz -r ../temp/{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-brain_mask.nii.gz -w ../temp/{sub}_ses-001_task-{task}_from-T1w_to-scanner_mode-image_xfm.nii.gz')
+        #print(f'{sub} {task} - Extracting timecourses...')
+        #os.system(f'3dROIstats -mask_f2short -mask ../temp/{sub}_{task}_Schaefer_resliced.nii.gz -nomeanout -numROI 400 -quiet -nzmean /home/annatruzzi/foundcog_adult_pilot_volumetric/temp/{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz > /home/annatruzzi/foundcog_adult_pilot_volumetric/temp/{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt')
+
+        os.system(f'fslmeants -i ../temp/{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz --label=tpl-MNI152NLin2009cAsym_res-01_atlas-Schaefer2018_desc-400Parcels17Networks_dseg.nii.gz -o ../temp/{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt')
+        #s3.upload_file(os.path.join(temp_pth, f'{sub}_{task}_Schaefer_resliced.nii.gz'), bucket, f'foundcog-adult-pilot-2/volumetric-timecourses/{sub}/{sub}_{task}_Schaefer_resliced.nii.gz')
+        s3.upload_file(os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt'), bucket, f'foundcog-adult-pilot-2/volumetric-timecourses/{sub}/{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt')
+
+
+
+        ## Extract timecourses with FSL
+        #print(f'Extracting timecourses - {sub} {task}')
+        #roifsl = fsl.ImageMeants()
+        #roifsl.inputs.in_file = os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz')
+#        roifsl.inputs.mask = Path('/dhcp/rhodri_registration/atlases/schaefer/Parcellations/MNI/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm.nii.gz')
+        #roifsl.inputs.label = Path(os.path.join(temp_pth, 'Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm_resliced.nii.gz'))
+        #roifsl.inputs.out_file = os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt')
+        #print(roifsl.cmdline)
+        #res = roifsl.run()
+        #s3.upload_file(os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt'), bucket, f'foundcog-adult-pilot-2/volumetric-timecourses/{sub}/{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt')
+
+
+        ## Extract timecourses with AFNI
+        #print(f'Extracting timecourses - {sub} {task}')
+        #roistats = afni.ROIStats()
+        #roistats.inputs.in_file = os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz')
+        #roistats.inputs.mask_file = Path('/dhcp/rhodri_registration/atlases/schaefer/Parcellations/MNI/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm.nii.gz')
+        #roistats.inputs.quiet=True
+        #roistats.inputs.mask_f2short = True
+        #roistats.inputs.num_ROI = 400
+        #roistats.inputs.nomeanout = True
+        #roistats.inputs.stat = ['mean']
+        #roistats.inputs.out_file = os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_Schaefer400_timecourses.txt')
+        #print(roistats.cmdline)
+        #res = roistats.run()
+        
+        #os.system('3dROIstats -mask_f2short -mask /dhcp/rhodri_registration/atlases/schaefer/Parcellations/MNI/Schaefer2018_400Parcels_17Networks_order_FSLMNI152_1mm.nii.gz -nomeanout -numROI 400 -quiet -nzmean /home/annatruzzi/foundcog_adult_pilot_volumetric/temp/sub-02_ses-001_task-resting_run-1_space-MNI152NLin2009cAsym_desc-preproc_bold.nii.gz > /home/annatruzzi/foundcog_adult_pilot_volumetric/temp/sub-02_ses-001_task-resting_run-1_Schaefer400_timecourses.txt')
+        
+        
+        ###################
+        # TENTATIVE STEPS #
+        ###################
+        '''s3.download_file(bucket, f'foundcog-adult-pilot-2/bids/{sub}/ses-001/func/{sub}_ses-001_task-{task}_bold.nii.gz', f'{sub}_ses-001_task-{task}_bold.nii.gz')
         s3.download_file(bucket, f'foundcog-adult-pilot-2/bids/{sub}/ses-001/anat/{sub}_ses-001_run-001_T1w.nii.gz', f'{sub}_ses-001_run-001_T1w.nii.gz')
         os.system(f'mv {sub}_ses-001_task-{task}_bold.nii.gz ../temp/')
         os.system(f'mv {sub}_ses-001_run-001_T1w.nii.gz ../temp/')
@@ -54,6 +124,7 @@ for sub in sub_list:
         flt.inputs.out_matrix_file = os.path.join(temp_pth, f'{sub}_ses-001_transform.mat')
         #res = flt.run()
 
+        print(f'{sub} - Working on functional registration...')
         flt_func = fsl.FLIRT()
         flt_func.inputs.in_file = Path(os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_bold_mcflirt.nii.gz'))
         flt_func.inputs.reference = os.path.join(temp_pth, f'{sub}_ses-001_T1w_RegisteredToMNI.nii.gz')
@@ -65,8 +136,10 @@ for sub in sub_list:
         s3.upload_file(os.path.join(temp_pth, f'{sub}_ses-001_task-{task}_bold_RegisteredToMNI.nii.gz'), bucket, f'foundcog-adult-pilot-2/volumetric_preprocessing/t1-coregistration/{sub}/{sub}_ses-001_task-{task}_bold_RegisteredToMNI.nii.gz')
         s3.upload_file(os.path.join(temp_pth, f'{sub}_ses-001_transform.mat'), bucket, f'foundcog-adult-pilot-2/volumetric_preprocessing/t1-coregistration/{sub}/{sub}_ses-001_transform.nii.gz')
         s3.upload_file(os.path.join(temp_pth, f'{sub}_ses-001_T1w_RegisteredToMNI.nii.gz'), bucket, f'foundcog-adult-pilot-2/volumetric_preprocessing/t1-coregistration/{sub}/{sub}_ses-001_T1w_RegisteredToMNI.nii.gz')
+        '''
 
         
+        #### ANTS registration
         '''reg = Registration()
         reg.inputs.fixed_image = Path('/dhcp/rhodri_registration/atlases/fsl/MNI152_T1_1mm_brain.nii.gz')
         reg.inputs.moving_image = os.path.join(temp_pth, f'{sub}_ses-001_T1w_bet.nii.gz')
